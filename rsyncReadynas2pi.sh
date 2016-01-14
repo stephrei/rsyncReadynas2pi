2dcc3990f4e5a5ce14b9bcf330208937cafff74e
stephrei <awaynothere@hotmail.com>
2016-01-14 13:48:06 +1100

    added list of error numbers

--- rsyncReadynas2pi.sh
+++ rsyncReadynas2pi.sh
@@ -6,10 +6,18 @@
 # usage: rsyncReadynas2pi.sh <src_path> <dst_path> <|dry>
 # awaynothere@hotmail.com 11jan16
 # version 1.0
+#
+# list of errors
+# no	description
+# 10	lock file exists	
+# 12	incorrect no of parameters	
+# 14	SRC and/or DST not writeable
+# 16	LOG and/or RSYNC_LOG not writeable
+# 30	rsync errors
+
 
 #TODO: 
-#rsync progress 
-#grep "rsync error" and email rsync errors
+#rsync progress indicator with --info=progress2 
 
 ### vars ###
 
@@ -42,9 +50,7 @@ function write2log() {
 
 function email_error() {
 	local MSG="$1"
-echo "MSG: $MSG"
 	local ERROR_NO="${2:-"Unknown Error"}"	
-echo "echo $MSG | $MAILER -s 'Error no $ERROR_NO with rsync scripts on $HOSTNAME at $(date "+%d%b %T") hrs' $EMAIL"
 	echo $MSG | $MAILER -s "Error no $ERROR_NO with rsync scripts on $HOSTNAME at $(date "+%d%b %T") hrs" $EMAIL
 }
 
@@ -53,11 +59,6 @@ function email_progress() {
 	local MSG="$2"
 	echo "$MSG" | $MAILER -s "Rsyncing on $HOSTNAME: progress = $PROGRESS%" $EMAIL
 }
-
-function extract_rsync_errors() {
-	grep "rsync error" $RSYNC_LOG
-}
-
 
 function set_lockfile() {
 	write2log "setting lock file"
@@ -85,7 +86,7 @@ fi
 if [ "$#" == "2" -o "$#" == "3" ]; then
 	SRCPATH="$1"; DSTPATH="$2"; DRY="$3"
 else
-	echo; echo "Incorrect number of arguments!"
+	email_error "Error rsyncing on $HOSTNAME: incorrect number of parameters supplied" "12"
 	write2log 'Incorrect parameters supplied: SRCPATH="$1"; DSTPATH="$2"; DRY="$3"'
 	write2log "exiting ..."
 	display_usage
@@ -101,9 +102,11 @@ fi
 
 # are SRC, DST set and LOG, RSYNC_LOG writeable?
 if [ "$SRC" == "" -o "$DST" == "" ]; then 
+	email_error "Error rsyncing on $HOSTAME: SRC and/or DST not writeable" "14"
 	write2log "SRC and/or DST not set, exiting ..."
 	exit 1
 else if [[ ! -f "$LOG" || ! -f "$RSYNC_LOG" ]]; then 
+	email_error "Error rsyncing on $HOSTNAME: LOG and/or RSYNC_LOG not writeable" "16"
 	write2log "LOG and/or RSYNC_LOG not writeable, exiting ..."
 	exit 1
 fi
@@ -119,7 +122,7 @@ email_progress "0" "starting rsync on $HOSTNAME"
 rsync -ratzv"$DRY" --exclude='lost+found' --exclude="*.Apple*" --exclude="*.DS_*" --log-file="$RSYNC_LOG" "$SRCPATH" "$DSTPATH" && write2log "... finished rsync"
 email_progress "100" "finished rsync on $HOSTNAME"
 
-# email possible rsync errors - error no 111
-email_error "$(grep 'rsync error' $RSYNC_LOG)" 111
+# email possible rsync errors - error no 30
+email_error "$(grep 'rsync error' $RSYNC_LOG)" "30"
 
 remove_lockfile
